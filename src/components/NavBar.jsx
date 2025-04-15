54c7cb7272c3ec62cd75160c14edad15da220fb8
Larry <larryotieno51@gmail.com>
2025-04-15 18:24:04 +0300

    Adds drouter routes lab

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,24 @@
-import { NavLink } from "react-router-dom";
 import "./NavBar.css";
+import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
     <nav className="navbar">
-    
-    </nav>
+    {/* <a href="/">Home</a>
+    <a href="/directors">Directors</a>
+    <a href="/actors">Actors</a> */
+    }
+    <NavLink to="/" className="nav-link">
+        Home
+      </NavLink>
+      <NavLink to="/directors" className="nav-link">
+        Directors
+      </NavLink>
+      <NavLink to="/actors" className="nav-link">
+        Actors
+      </NavLink>
+
+  </nav>
     );
 };
 
